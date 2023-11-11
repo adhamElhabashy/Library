833ca9b2c432222131b46ffbb840fce075f6acaa
adhamElhabashy <adhammohamed8321@gmail.com>
2023-11-11 20:32:52 +0200

    feature: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 import {
 	createTheme,
 	responsiveFontSizes,
 	ThemeProvider,
 } from "@mui/material/styles";
-import { Button } from "@mui/material";
+import Header from "./Components/Header/Header";
 
 const theme = createTheme({
 	palette: {
-		primary: { main: "#C38654", light: "rgba(195, 134, 84, 0.15)" },
+		primary: { main: "rgba(195, 134, 84, 0.15)", dark: "#C38654" },
 		secondary: { main: "rgba(0, 0, 0, 0.49)" },
 	},
 	typography: {
@@ -20,33 +18,9 @@ const theme = createTheme({
 });
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<ThemeProvider theme={responsiveFontSizes(theme)}>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<Button variant="contained" sx={{ bgcolor: "primary.light" }}>
-					button
-				</Button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<Header />
 		</ThemeProvider>
 	);
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,112 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import {
+	BookSharp,
+	Bookmarks,
+	Interests,
+	MenuBook,
+	Search,
+	Settings,
+} from "@mui/icons-material";
+
+function Header() {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="fixed">
+				<Toolbar sx={{ color: "primary.dark" }}>
+					<IconButton color="inherit" aria-label="logo icon" size="large">
+						<MenuBook fontSize="large" />
+					</IconButton>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						The Library
+					</Typography>
+					<div>
+						<IconButton
+							size="large"
+							color="inherit"
+							aria-label="open drawer"
+							aria-controls={open ? "basic-menu" : undefined}
+							aria-haspopup="true"
+							aria-expanded={open ? "true" : undefined}
+							onClick={handleClick}
+						>
+							<AccountCircle />
+						</IconButton>
+					</div>
+				</Toolbar>
+				<Menu
+					id="basic-menu"
+					anchorEl={anchorEl}
+					open={open}
+					onClose={handleClose}
+					MenuListProps={{
+						"aria-labelledby": "menu-button",
+					}}
+					sx={{ marginRight: "30px !important" }}
+				>
+					<MenuItem onClick={handleClose}>
+						<Interests
+							className="menu-icons"
+							color="primary.dark"
+							sx={{ paddingRight: 1, color: "primary.dark" }}
+						/>
+						For You
+					</MenuItem>
+					<MenuItem onClick={handleClose}>
+						<BookSharp
+							className="menu-icons"
+							sx={{ paddingRight: 1, color: "primary.dark" }}
+						/>
+						Trending Books
+					</MenuItem>
+					<MenuItem onClick={handleClose}>
+						<Search
+							className="menu-icons"
+							sx={{ paddingRight: 1, color: "primary.dark" }}
+						/>
+						Search
+					</MenuItem>
+					<MenuItem onClick={handleClose}>
+						<EmojiEventsIcon
+							className="menu-icons"
+							sx={{ color: "primary.dark" }}
+						/>
+						Challenges
+					</MenuItem>
+					<MenuItem onClick={handleClose}>
+						<Bookmarks
+							className="menu-icons"
+							sx={{ paddingRight: 1, color: "primary.dark" }}
+						/>
+						Your Books
+					</MenuItem>
+					<MenuItem onClick={handleClose}>
+						<Settings
+							className="menu-icons"
+							sx={{ paddingRight: 1, color: "primary.dark" }}
+						/>
+						Settings
+					</MenuItem>
+				</Menu>
+			</AppBar>
+		</Box>
+	);
+}
+
+export default Header;
