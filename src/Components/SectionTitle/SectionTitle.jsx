61b32d3fb084a54fd391644d70a55a9f973f8f7c
adhamElhabashy <adhammohamed8321@gmail.com>
2023-11-15 21:21:31 +0200

    feature: add section title component

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function SectionTitle() {
+	return (
+		<Typography
+			variant="h3"
+			sx={{ color: "primary.dark", padding: 5 }}
+			align="center"
+		>
+			Web Development
+		</Typography>
+	);
+}
+
+export default SectionTitle;
