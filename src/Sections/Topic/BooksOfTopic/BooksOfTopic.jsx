b23107417479918884ba0e50badc2b197c8d9ca4
adhamElhabashy <adhammohamed8321@gmail.com>
2023-11-18 20:37:22 +0200

    feature: add Pagination component

--- src/Sections/Topic/BooksOfTopic/BooksOfTopic.jsx
+++ src/Sections/Topic/BooksOfTopic/BooksOfTopic.jsx
@@ -2,13 +2,21 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import BookCard from "../../../Components/BookCard/BookCard";
 import { Container, Grid } from "@mui/material";
+import Pagination from "@mui/material/Pagination";
 
 function BooksOfTopic() {
 	const [books, setBooks] = useState([]);
 	const booksData = useSelector((state) => state.books);
+	const [page, setPage] = React.useState(1);
+
 	useEffect(() => {
 		setBooks(booksData.data);
 	}, [booksData]);
+
+	const handleChange = (event, value) => {
+		setPage(value);
+	};
+
 	if (books?.length !== 0) {
 		return (
 			<Container className="books-list">
@@ -31,6 +39,17 @@ function BooksOfTopic() {
 						);
 					})}
 				</Grid>
+				<Pagination
+					count={10}
+					page={page}
+					onChange={handleChange}
+					sx={{
+						mt: 5,
+						position: "absolute",
+						left: "50%",
+						transform: "translateX(-50%)",
+					}}
+				/>
 			</Container>
 		);
 	}
