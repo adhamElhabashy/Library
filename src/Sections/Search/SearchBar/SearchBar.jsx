658456ea28e55004d61e1dd0cd957dbdab38f473
adhamElhabashy <adhammohamed8321@gmail.com>
2023-12-18 22:36:49 +0200

    feature: add search bar

--- src/Sections/Search/SearchBar/SearchBar.jsx
+++ src/Sections/Search/SearchBar/SearchBar.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import "./SearchBar.css";
+import { useTheme } from "@emotion/react";
+import { Button } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { fetchBooksSearch } from "../../../Features/BooksSearchSlice";
 
-function SearchBar() {}
+function SearchBar() {
+	// added search functionality
+	// need to solve responsiveness
+	const theme = useTheme();
+	const dispatch = useDispatch();
+	const inputRef = useRef();
+	function searchForBook(value) {
+		dispatch(fetchBooksSearch(value));
+	}
+
+	return (
+		<div className="search-bar">
+			<input
+				type="text"
+				style={{ backgroundColor: theme.palette.primary.main }}
+				className="search-input"
+				ref={inputRef}
+			/>
+			<Button
+				variant="contained"
+				sx={{
+					borderRadius: 25,
+					padding: "15px 25px",
+					position: "absolute",
+					border: "1px solid ",
+					left: "65%",
+					borderColor: "primary.dark",
+					color: "primary.dark",
+					fontWeight: "bold",
+					":hover": {
+						color: "common.white",
+					},
+				}}
+				onClick={(event) => searchForBook(inputRef.current.value)}
+			>
+				Search
+			</Button>
+		</div>
+	);
+}
 
 export default SearchBar;
