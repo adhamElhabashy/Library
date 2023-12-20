38a414bf09322231fa7642ad42b908034ca098c0
adhamElhabashy <adhammohamed8321@gmail.com>
2023-12-20 22:29:13 +0200

    fix: add updating the book object status

--- src/Functions/SaveInLocalStorage.js
+++ src/Functions/SaveInLocalStorage.js
@@ -1,14 +1,28 @@
 export function saveInLocalStorage(object, readingStatusParam) {
 	let storageArray = [];
+
 	const savedBooks = window.localStorage.getItem("saved-books");
+
 	if (savedBooks) {
 		storageArray = JSON.parse(savedBooks);
 	}
 
-	storageArray.push({
-		...object,
-		readingStatus: readingStatusParam,
-	});
+	const theLocalStorageObject = storageArray.find(
+		(ele) => ele.id === object.id
+	);
+
+	// search in the localStorage json for the same object as the selected book
+	if (theLocalStorageObject) {
+		storageArray.splice(storageArray.indexOf(theLocalStorageObject), 1, {
+			...object,
+			readingStatus: readingStatusParam,
+		});
+	} else {
+		storageArray.push({
+			...object,
+			readingStatus: readingStatusParam,
+		});
+	}
 
 	let jsonObject = JSON.stringify(storageArray);
 
