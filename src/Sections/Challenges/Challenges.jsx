6027118ffca58bf05de00ca8726279c38f6e6841
adhamElhabashy <adhammohamed8321@gmail.com>
2024-01-04 23:22:47 +0200

    fix: center the default message

--- src/Sections/Challenges/Challenges.jsx
+++ src/Sections/Challenges/Challenges.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import ChallengesList from "./ChallengesList/ChallengesList";
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import SavedBooksContext from "../../Context/SavedBooksContext";
 
 function Challenges() {
+	const savedBooksContext = useContext(SavedBooksContext);
 	return (
 		<div className="challenges">
-			<Box sx={{ marginTop: "50px", bgcolor: "primary.main" }}>
+			<Box
+				sx={{
+					marginTop: "50px",
+					padding: "30px 0",
+					bgcolor: "primary.main",
+					minHeight: "100vh",
+					position: "relative",
+				}}
+			>
 				<SectionTitle>Your Challenges and Saved Books</SectionTitle>
-				<ChallengesList />
+				{savedBooksContext.savedBooks ? (
+					<ChallengesList />
+				) : (
+					<Typography
+						variant="h5"
+						sx={{
+							position: "absolute",
+							top: "50%",
+							left: "50%",
+							transform: "translate(-50%, -50%)",
+							color: "primary.dark",
+						}}
+					>
+						No Book Saved Yet!
+					</Typography>
+				)}
 			</Box>
 		</div>
 	);
