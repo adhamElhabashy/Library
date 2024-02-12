3121b6c5a89640959b496a5399d1cc1612840f83
adhamElhabashy <adhammohamed8321@gmail.com>
2024-02-12 22:27:01 +0200

    feature: add useBookWithId hook to book page

--- src/Components/BookPage/BookPage.jsx
+++ src/Components/BookPage/BookPage.jsx
@@ -1,31 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import BookCard from "../BookCard/BookCard";
-import axios from "axios";
+import { useBookWithId } from "../../Hooks/useBookWithId.js";
 
 function BookPage() {
 	const { bookId } = useParams();
-	const [book, setBook] = useState();
-	async function getTheBookWith(id) {
-		const response = await axios.get(
-			`https://www.googleapis.com/books/v1/volumes?q=${id}&key=${
-				import.meta.env.VITE_GOOGLE_BOOKS_API_KEY
-			}`
-		);
-		setBook(() => response.data?.items[0]);
-		console.log(response.data?.items[0]);
-	}
-	useEffect(() => {
-		getTheBookWith(bookId);
-	}, []);
-	return (
-		<BookCard
-			object={book}
-			thumbnail={book?.volumeInfo.imageLinks?.thumbnail}
-			title={book?.volumeInfo.title}
-			authors={book?.volumeInfo?.authors?.join(", ")}
-		/>
-	);
+	const book = useBookWithId(bookId);
+
+	return <img src={book?.volumeInfo.imageLinks?.thumbnail} alt="" />;
 }
 
 export default BookPage;
