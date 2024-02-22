4b6efcefb89fd39d7529bbd26bf1aefc78f20056
adhamElhabashy <adhammohamed8321@gmail.com>
2024-02-22 21:34:45 +0200

    fix: improve landing page responsivity with MUI grid

--- src/Sections/Landing/Landing.jsx
+++ src/Sections/Landing/Landing.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import Shelf from "./shelf/Shelf";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 function Landing() {
 	return (
 		<Box className="landing" sx={{ height: "100vh", bgcolor: "primary.main" }}>
-			<Container
+			<Grid
+				container
 				sx={{
 					height: "100%",
-					display: "flex",
-					justifyContent: "center",
 					alignItems: "center",
+					justifyContent: "center",
 				}}
 			>
-				<Typography
-					variant="h3"
-					sx={{ color: "primary.dark" }}
-					textAlign="left"
-				>
-					Bring Your Coffee, And Start Your Reading Journey
-				</Typography>
-				<Shelf />
-			</Container>
+				<Grid item md={8}>
+					<Typography
+						variant="h3"
+						sx={{ color: "primary.dark", padding: "10px" }}
+						textAlign="center"
+					>
+						Bring Your Coffee, And Start Your Reading Journey
+					</Typography>
+				</Grid>
+				<Grid item md={4}>
+					<Shelf />
+				</Grid>
+			</Grid>
 		</Box>
 	);
 }
