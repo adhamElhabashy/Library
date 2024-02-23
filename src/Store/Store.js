357cb2361aec48fceb1ef5012331be5568e2b9db
adhamElhabashy <adhammohamed8321@gmail.com>
2024-02-23 21:28:35 +0200

    Production

--- src/Store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import WebBooksReducer from "../Features/WebBooksSlice";
-import BooksSearchReducer from "../Features/BooksSearchSlice";
-
-const store = configureStore({
-	reducer: { books: WebBooksReducer, booksSearch: BooksSearchReducer },
-});
-
-export default store;
