5372e70762368dffa4f19cf28cfb4fe246c58cdc
adhamElhabashy <adhammohamed8321@gmail.com>
2024-02-25 22:17:31 +0200

    fix: edit useBookWithId to prevent not found errors

--- src/Components/BookPage/BookPage.jsx
+++ src/Components/BookPage/BookPage.jsx
@@ -7,8 +7,15 @@ import { Delete } from "@mui/icons-material";
 
 function BookPage() {
 	const { bookId } = useParams();
+	const [book, setBook] = useState(null);
 
-	const [book, setBook] = useState(useBookWithId(bookId));
+	useEffect(() => {
+		useBookWithId(bookId)
+			.then((bookData) => setBook(bookData))
+			.catch((error) => {
+				console.error("Error fetching book:", error);
+			});
+	}, [bookId]);
 
 	const localStorageArray = JSON.parse(
 		window.localStorage.getItem("saved-books")
@@ -30,72 +37,80 @@ function BookPage() {
 		window.localStorage.setItem("saved-books", jsonObject);
 	}
 
-	return (
-		<Box
-			className="book-page"
-			sx={{
-				bgcolor: "primary.main",
-			}}
-		>
-			<Container className="book-page-container">
-				<div className="book-description">
-					<div className="image-holder">
-						<img
-							src={book?.volumeInfo.imageLinks?.thumbnail}
-							alt={book?.volumeInfo.title}
-						/>
-					</div>
-					<div className="book-details">
-						<Typography variant="h2" color={"primary.dark"} gutterBottom>
-							{book?.volumeInfo.title}
-						</Typography>
-						<Typography variant="h6" color={"primary.dark"} gutterBottom>
-							Authors: {book?.volumeInfo.authors.join(", ")}
-						</Typography>
-						<Typography variant="subtitle1" color={"primary.dark"} gutterBottom>
-							{book?.volumeInfo.description}
-						</Typography>
-						<div className="sub-details">
-							<Typography variant="h6" color={"primary.dark"}>
-								Categories: {book?.volumeInfo.categories.join(", ")}
+	if (book) {
+		return (
+			<Box
+				className="book-page"
+				sx={{
+					bgcolor: "primary.main",
+				}}
+			>
+				<Container className="book-page-container">
+					<div className="book-description">
+						<div className="image-holder">
+							<img
+								src={book?.volumeInfo.imageLinks?.thumbnail}
+								alt={book?.volumeInfo.title}
+							/>
+						</div>
+						<div className="book-details">
+							<Typography variant="h2" color={"primary.dark"} gutterBottom>
+								{book?.volumeInfo.title}
 							</Typography>
-							<Typography variant="h6" color={"primary.dark"}>
-								pages: {book?.volumeInfo.pageCount}
+							<Typography variant="h6" color={"primary.dark"} gutterBottom>
+								Authors: {book?.volumeInfo.authors.join(", ")}
 							</Typography>
-							<Typography variant="h6" color={"primary.dark"}>
-								Published: {book?.volumeInfo.publishedDate}
+							<Typography
+								variant="subtitle1"
+								color={"primary.dark"}
+								gutterBottom
+							>
+								{book?.volumeInfo.description}
 							</Typography>
+							<div className="sub-details">
+								<Typography variant="h6" color={"primary.dark"}>
+									Categories: {book?.volumeInfo.categories.join(", ")}
+								</Typography>
+								<Typography variant="h6" color={"primary.dark"}>
+									pages: {book?.volumeInfo.pageCount}
+								</Typography>
+								<Typography variant="h6" color={"primary.dark"}>
+									Published: {book?.volumeInfo.publishedDate}
+								</Typography>
+							</div>
 						</div>
 					</div>
-				</div>
-				{book?.notes && (
-					<div className="notes">
-						<Typography
-							variant="h3"
-							textAlign={"center"}
-							color={"primary.dark"}
-						>
-							Notes
-						</Typography>
-						{book?.notes.map((ele, index) => (
-							<div className="note" key={index}>
-								<Typography variant="string" color={"primary.dark"}>
-									{ele.note}
-								</Typography>
-								<IconButton
-									onClick={() => {
-										removeNote(ele.number);
-									}}
-								>
-									<Delete sx={{ color: "primary.dark" }} />
-								</IconButton>
-							</div>
-						))}
-					</div>
-				)}
-			</Container>
-		</Box>
-	);
+					{book?.notes && (
+						<div className="notes">
+							<Typography
+								variant="h3"
+								textAlign={"center"}
+								color={"primary.dark"}
+							>
+								Notes
+							</Typography>
+							{book?.notes.map((ele, index) => (
+								<div className="note" key={index}>
+									<Typography variant="string" color={"primary.dark"}>
+										{ele.note}
+									</Typography>
+									<IconButton
+										onClick={() => {
+											removeNote(ele.number);
+										}}
+									>
+										<Delete sx={{ color: "primary.dark" }} />
+									</IconButton>
+								</div>
+							))}
+						</div>
+					)}
+				</Container>
+			</Box>
+		);
+	} else {
+		return <h2>Something went wrong</h2>;
+	}
 }
 
 export default BookPage;
--- src/Hooks/useBookWithId.js
+++ src/Hooks/useBookWithId.js
@@ -1,25 +1,12 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { useState } from "react";
 
 export function useBookWithId(id) {
-	const [response, setResponse] = useState();
-
-	const savedBooksFromLocalStorage = JSON.parse(
-		window.localStorage.getItem("saved-books")
-	);
-	const checkObject = savedBooksFromLocalStorage.find((ele) => ele.id === id);
-	if (checkObject) {
-		return checkObject;
-	} else {
-		axios
-			.get(
-				`https://www.googleapis.com/books/v1/volumes?q=${id}&key=${
-					import.meta.env.VITE_GOOGLE_BOOKS_API_KEY
-				}`
-			)
-			.then((response) => {
-				setResponse(() => response?.data.items[0]);
-			});
-		return response;
-	}
+	return axios
+		.get(
+			`https://www.googleapis.com/books/v1/volumes/${id}?key=${
+				import.meta.env.VITE_GOOGLE_BOOKS_API_KEY
+			}`
+		)
+		.then((response) => response.data);
 }
